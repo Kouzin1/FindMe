23fa2d463d680892d1c5249440668ede359cf44b
kouzin1 <samuelakinwunmi2@gmail.com>
2024-01-30 17:09:31 +0000

    Update

--- script.js
+++ script.js
@@ -39,6 +39,12 @@ class Cycling extends Workout {
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
+    this.CalcSpeed();
+  }
+
+  CalcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
   }
 }
 
